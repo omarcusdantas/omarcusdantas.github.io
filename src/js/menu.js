0a39b050fe1540d0fb0d61b862f910b2ba403d11
MarcusDantas <marvinsd@gmail.com>
2023-09-07 14:45:43 -0300

    feat: add resposive header with menu

---
+++ src/js/menu.js
@@ -0,0 +1,42 @@
+// Menu variables
+let nav, menu, menuButton, notMenu;
+const timeMenuAnimation = 250;
+
+function toggleMenuHeight() {
+    menu.classList.toggle("menu-height");
+}
+
+function toggleNav() {
+    nav.classList.toggle("show");
+}
+
+function disableMenu() {
+    toggleMenuHeight();
+    setTimeout(toggleNav, timeMenuAnimation);
+    notMenu.removeEventListener("click", disableMenu);
+}
+
+function setDisableMenu() {
+    notMenu.addEventListener("click", disableMenu);
+}
+
+function setHamburgerMenu() {
+    menuButton.addEventListener("click", function () {
+        if (nav.classList.contains("show")) {
+            disableMenu();
+            return;
+        }
+        toggleNav();
+        setTimeout(toggleMenuHeight, 1);
+        setTimeout(setDisableMenu, timeMenuAnimation);
+    });
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+    nav = document.querySelector("nav");
+    menu = document.querySelector("nav ul");
+    menuButton = document.querySelector(".hamburger-menu");
+    notMenu = document.querySelector("body");
+
+    setHamburgerMenu();
+});
